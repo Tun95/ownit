877066d8a7b2f672f38fbb25d6cf68a776ec0e92
Tunji Akande <akandetunji3@gmail.com>
2024-11-02 23:49:58 +0100

    FEAT: vercel live

--- frontend/src/components/form/register/Register.jsx
+++ frontend/src/components/form/register/Register.jsx
@@ -24,6 +24,7 @@ const initialRegisterValues = {
   confirmPassword: "",
 };
 
+
 function RegisterComponent() {
   const navigate = useNavigate();
   const { state: appState, dispatch: ctxDispatch } = useAppContext();
