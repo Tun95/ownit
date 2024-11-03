2815284ae074c65b4901edd1ac8b06acb7118175
Tunji Akande <akandetunji3@gmail.com>
2024-11-03 13:27:28 +0100

    FEAT:vercel live

--- backend/utils.js
+++ backend/utils.js
@@ -18,7 +18,7 @@ export const generateToken = (user) => {
     },
     process.env.JWT_SECRET || "somethingsecret",
     {
-      expiresIn, // Use the determined expiration time
+      expiresIn,
     }
   );
 };
